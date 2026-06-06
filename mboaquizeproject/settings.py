7cb89af9c93e6fbe69072f2d40290c1cbec32652
TchambaMSK <160991655+TchambaMSK@users.noreply.github.com>
2026-06-06 07:01:37 +0100

    Set DEBUG to False and allow all hosts

--- mboaquizeproject/settings.py
+++ mboaquizeproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%w4xeie&^jy9%rui@wn0cxosoj1gx)(y)7(l3lst#5!_93m9t8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
